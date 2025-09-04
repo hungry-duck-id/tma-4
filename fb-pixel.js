6edb57882c0183441b4bc00310bd6edcdfbd8044
hungry-duck-id <a26264634@proton.me>
2025-09-04 02:01:59 -0700

    Update fb-pixel.js

--- fb-pixel.js
+++ fb-pixel.js
@@ -6,5 +6,5 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1421219145688744');
+fbq('init', '1447484416489037');
 fbq('track', 'PageView');
